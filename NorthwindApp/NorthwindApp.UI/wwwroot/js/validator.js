5beee0496fb1a971089f0a3a6cd83f5e03408e95
Natalia Vladimirova <natalia.vladimirova.96@gmail.com>
2019-03-09 22:14:24 +0300

    Module 01 Task 4 - use adapters.addSingleVal

--- NorthwindApp/NorthwindApp.UI/wwwroot/js/validator.js
+++ NorthwindApp/NorthwindApp.UI/wwwroot/js/validator.js
@@ -1,12 +1,7 @@
-﻿$.validator.addMethod("greaterThanOrEqual", function (value, element, params) {
-    var minValue = params[0];
+﻿$.validator.addMethod("greaterThanOrEqual", function (value, element, param) {
+    var minValue = parseInt(param);
 
-    return value && value >= minValue;
+    return value && parseInt(value) >= minValue;
 });
+$.validator.unobtrusive.adapters.addSingleVal("greaterThanOrEqual", "minValue");
 
-$.validator.unobtrusive.adapters.add("greaterThanOrEqual",
-    ["minValue"],
-    function (options) {
-        options.rules["greaterThanOrEqual"] = [parseInt(options.params["minValue"])];
-        options.messages["greaterThanOrEqual"] = options.message;
-    });

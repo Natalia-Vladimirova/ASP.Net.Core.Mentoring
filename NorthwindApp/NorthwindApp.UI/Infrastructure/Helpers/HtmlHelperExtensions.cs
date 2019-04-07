b953a285ffa10d7a35db0462c80a40b0198e1d62
Natalia Vladimirova <natalia.vladimirova.96@gmail.com>
2019-04-07 17:53:27 +0300

    Module 03 Task 3 - create html and tag helpers for categories images links

---
+++ NorthwindApp/NorthwindApp.UI/Infrastructure/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NorthwindApp.UI.Infrastructure.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper html, int id, string linkText)
+        {
+            return html.NorthwindImageLink(id, linkText, null);
+        }
+
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper html, int id, string linkText, object htmlAttributes)
+        {
+            return html.ActionLink(linkText, "Image", "Category", new { id }, htmlAttributes);
+        }
+    }
+}
---
+++ NorthwindApp/NorthwindApp.UI/Infrastructure/Helpers/NorthwindImageTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace NorthwindApp.UI.Infrastructure.Helpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-id", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class NorthwindImageTagHelper : TagHelper
+    {
+        private readonly IUrlHelperFactory _urlHelperFactory;
+
+        public NorthwindImageTagHelper(IUrlHelperFactory helperFactory)
+        {
+            _urlHelperFactory = helperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public int NorthwindId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+
+            output.Attributes.SetAttribute("href", urlHelper.Action("Image", "Category", new { id = NorthwindId }));
+        }
+    }
+}

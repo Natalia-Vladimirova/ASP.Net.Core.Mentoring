75710a297ec650c47339efec88db8d25b74ebee6
Natalia Vladimirova <natalia.vladimirova.96@gmail.com>
2019-04-21 13:52:50 +0300

    Module 04 Task 1 - add API controllers to get products and categories

---
+++ NorthwindApp/NorthwindApp.Api/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindApp.Models;
+using NorthwindApp.Services.Interfaces;
+
+namespace NorthwindApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> Get(int page, int pageSize)
+        {
+            if (page < 0 || pageSize < 0)
+            {
+                return BadRequest();
+            }
+
+            var products = await _productService.GetProductsAsync(page, pageSize);
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> Get(int id)
+        {
+            var product = await _productService.GetProductAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
+}
---
+++ NorthwindApp/NorthwindApp.Api/Startup.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NorthwindApp.DAL.Infrastructure;
+using NorthwindApp.DAL.Interfaces;
+using NorthwindApp.DAL.Repositories;
+using NorthwindApp.Services;
+using NorthwindApp.Services.Interfaces;
+using ConfigurationProvider = NorthwindApp.Core.Providers.ConfigurationProvider;
+using IConfigurationProvider = NorthwindApp.Core.Interfaces.IConfigurationProvider;
+
+namespace NorthwindApp.Api
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<NorthwindDbContext>(
+                options => options.UseSqlServer(_configuration.GetConnectionString("NorthwindConnection")));
+            services.AddScoped<DbContext>(x => x.GetService<NorthwindDbContext>());
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddSingleton<IConfigurationProvider, ConfigurationProvider>();
+
+            services.AddAutoMapper();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseMvc();
+        }
+    }
+}

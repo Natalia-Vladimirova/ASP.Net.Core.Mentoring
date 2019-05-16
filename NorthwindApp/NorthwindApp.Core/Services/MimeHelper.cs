77f86a8198af4c453ce85a769808ba3b05b76cc4
Natalia Vladimirova <Natalia_Vladimirova@epam.com>
2019-05-16 13:04:10 +0300

    Module 02 Task 2 - fix error when defining mime types

--- NorthwindApp/NorthwindApp.Core/Services/MimeHelper.cs
+++ NorthwindApp/NorthwindApp.Core/Services/MimeHelper.cs
@@ -1,13 +1,15 @@
-﻿using HeyRed.Mime;
+﻿using MimeDetective;
 using NorthwindApp.Core.Interfaces;
 
 namespace NorthwindApp.Core.Services
 {
     public class MimeHelper : IMimeHelper
     {
+        private const string DefaultMimeType = "application/octet-stream";
+
         public string GetMimeType(byte[] content)
         {
-            return MimeGuesser.GuessMimeType(content);
+            return content?.GetFileType()?.Mime ?? DefaultMimeType;
         }
     }
 }
